7c57d359898bcfe9b89f9d607698d8fbcc1c8eea
Thun (MacBook Pro) <bsthun@gmail.com>
2022-11-01 15:48:05 +0700

    Update frontend environement

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import * as fs from 'fs';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-})
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://127.0.0.1:8080',
+				changeOrigin: true,
+			},
+		},
+		https: {
+			key: fs.readFileSync('./res/key.pem'),
+			cert: fs.readFileSync('./res/cert.pem'),
+		},
+	},
+});
